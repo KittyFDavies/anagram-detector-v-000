1d4a2ff2995ba17d3ddda4e0d38d44908afce40d
Kitty Davies <kittyfdavies@gmail.com>
2018-03-28 20:47:35 +0000

    automatically backed up by learn

--- scratchpaper.rb
+++ scratchpaper.rb
@@ -1,8 +1,14 @@
-array.each do |item|
-  item = item.split("")
-  if item.sort == word_s.sort
-    item = item.join
-    @@all << item
+def match(array)
+  word_s = word.split("")
+  array.each do |item|
+    item = item.split("")
+    if item.sort == word_s.sort
+      item = item.join
+      @@all << item
+    end
   end
+  @@all
 end
-@@all
+
+
+
