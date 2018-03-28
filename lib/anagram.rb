08e776b7ae072ac41346321c3f421603eaa3e0f8
Kitty Davies <kittyfdavies@gmail.com>
2018-03-28 20:44:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,10 +16,10 @@ class Anagram
       item = item.split("")
       if item.sort == word_s.sort
         item = item.join
-        item
+        @@all << item
       end
     end
-
+    @@all
   end
 
 end
