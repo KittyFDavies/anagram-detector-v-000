1bb6b6f37bc1b04a4b1777e6c8fe9d7fe145bd62
Kitty Davies <kittyfdavies@gmail.com>
2018-03-28 20:45:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,14 +12,5 @@ class Anagram
 
   def match(array)
     word_s = word.split("")
-    array.each do |item|
-      item = item.split("")
-      if item.sort == word_s.sort
-        item = item.join
-        @@all << item
-      end
-    end
-    @@all
-  end
-
+    word_s.sort
 end
